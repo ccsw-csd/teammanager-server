e33817effaa2b0b6d61b32203c5b28c6bf12b271
CORP\apastorm <albert.pastor-martinez-external@capgemini.com>
2021-04-16 11:54:19 +0200

    Finalizada la parte back F-05.1

---
+++ src/main/java/com/capgemini/coedevon/teammanager/center/ListadoCentrosFestivosRepository.java
@@ -0,0 +1,17 @@
+package com.capgemini.coedevon.teammanager.center;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.capgemini.coedevon.teammanager.center.model.ListadoCentrosFestivosEntity;
+
+/**
+ * TODO apastorm This type ...
+ *
+ */
+public interface ListadoCentrosFestivosRepository extends CrudRepository<ListadoCentrosFestivosEntity, Long> {
+
+  @Override
+  List<ListadoCentrosFestivosEntity> findAll();
+}
