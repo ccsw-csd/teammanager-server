df06e367c7bf86634f9cacaeeda21d52b9fa277c
AOlmosCabrelles <a.olmoscabrelles@gmail.com>
2021-05-05 10:32:08 +0200

    Generacion de una auditoria en los cambios de festivos en centros

--- src/main/java/com/capgemini/coedevon/teammanager/centerFestive/CenterFestiveServiceImpl.java
+++ src/main/java/com/capgemini/coedevon/teammanager/centerFestive/CenterFestiveServiceImpl.java
@@ -1,14 +1,18 @@
 package com.capgemini.coedevon.teammanager.centerFestive;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.capgemini.coedevon.teammanager.center.CenterRepository;
 import com.capgemini.coedevon.teammanager.centerFestive.model.CenterFestiveEntity;
 import com.capgemini.coedevon.teammanager.centerFestive.model.CenterFestiveSaveDto;
+import com.capgemini.coedevon.teammanager.centerFestive.model.FestiveAuditEntity;
+import com.capgemini.coedevon.teammanager.config.security.UserUtils;
 
 /**
  *
@@ -19,6 +23,12 @@ public class CenterFestiveServiceImpl implements CenterFestiveService {
   @Autowired
   CenterFestiveRepository centerFestiveRepository;
 
+  @Autowired
+  FestiveAuditRepository festiveAuditRepository;
+
+  @Autowired
+  CenterRepository centerRepository;
+
   /**
    * {@inheritDoc}
    */
@@ -26,6 +36,10 @@ public class CenterFestiveServiceImpl implements CenterFestiveService {
   public List<CenterFestiveEntity> find(long centerId, int year) {
 
     return this.centerFestiveRepository.findAllByCenterIdAndYear(centerId, year);
+
+  }
+
+  private void saveLog(CenterFestiveSaveDto dto) {
 
   }
 
@@ -38,6 +52,9 @@ public class CenterFestiveServiceImpl implements CenterFestiveService {
     int year = dto.getYear();
     long centerId = dto.getCenterid();
     List<Date> dates = dto.getDates();
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    String user = UserUtils.getUserDetails().getUsername();
+    String log = "";
 
     this.centerFestiveRepository.deleteAllByCenterIdAndYear(centerId, year);
 
@@ -49,8 +66,18 @@ public class CenterFestiveServiceImpl implements CenterFestiveService {
       Calendar calendar = Calendar.getInstance();
       calendar.setTime(dates.get(i));
       festivo.setMonth(calendar.get(Calendar.MONTH) + 1);
+      log = log + festivo.getDate().getDay() + "/" + festivo.getMonth() + ",";
       this.centerFestiveRepository.save(festivo);
     }
+    log = log.substring(0, log.length() - 1);
+    FestiveAuditEntity audit = new FestiveAuditEntity();
+    audit.setCenterId(centerId);
+    audit.setDate(timestamp);
+    audit.setLog(log);
+    audit.setMonth(timestamp.getMonth());
+    audit.setYear(year);
+    audit.setUsername(user);
+    this.festiveAuditRepository.save(audit);
 
   }
 }
---
+++ src/main/java/com/capgemini/coedevon/teammanager/centerFestive/FestiveAuditRepository.java
@@ -0,0 +1,13 @@
+package com.capgemini.coedevon.teammanager.centerFestive;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.capgemini.coedevon.teammanager.centerFestive.model.FestiveAuditEntity;
+
+/**
+ * @author aolmosca
+ *
+ */
+public interface FestiveAuditRepository extends CrudRepository<FestiveAuditEntity, String> {
+
+}
