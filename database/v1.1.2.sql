384e7a8bc47cb4e3077398a6a9e82829f38256f3
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2021-05-20 18:24:56 +0200

    Update v1.1.2.sql

--- database/v1.1.2.sql
+++ database/v1.1.2.sql
@@ -1,3 +1,8 @@
+
+ALTER TABLE team_management.release_user ADD last_connection varchar(50) NULL;
+
+
+
 INSERT INTO team_management.release_note(version, text, `role`)
 VALUES('1.1.2', '<i>Feature</i>: Change in the order of the forecast list. It is now sorted by first name + surname.', 'GESTOR');
 
