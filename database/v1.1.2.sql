6bd80a842bac62c36585b395362758844c089e1e
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2021-05-20 18:22:14 +0200

    Add new file

---
+++ database/v1.1.2.sql
@@ -0,0 +1,6 @@
+INSERT INTO team_management.release_note(version, text, `role`)
+VALUES('1.1.2', '<i>Feature</i>: Change in the order of the forecast list. It is now sorted by first name + surname.', 'GESTOR');
+
+
+INSERT INTO team_management.release_note(version, text, `role`)
+VALUES('1.1.2', '<i>Feature</i>: Change in user permissions. Any person with grade D, E and F by default has the role of MANAGER.', 'GESTOR');
