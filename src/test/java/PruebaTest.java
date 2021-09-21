f9cfe8f14ef8bea7979b5a018ca25688fe3d2ad6
Pablo Jimenez Martinez <pablo.jimenez-martinez@capgemini.com>
2021-09-21 14:51:50 +0200

    Test de prueba

---
+++ src/test/java/PruebaTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.capgemini.coedevon.teammanager.utils.UtilsServiceImpl;
+
+public class PruebaTest {
+
+  @Test
+  public void Prueba() {
+
+    UtilsServiceImpl service = new UtilsServiceImpl();
+    Assert.assertEquals("1.3.1", service.getVersion());
+  }
+}
