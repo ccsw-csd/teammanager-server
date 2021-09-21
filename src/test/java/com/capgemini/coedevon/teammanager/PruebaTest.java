09bd4e366003a653cca79b604765bcb6495a54bf
Pablo Jimenez Martinez <pablo.jimenez-martinez@capgemini.com>
2021-09-21 14:58:01 +0200

    Test de prueba

--- src/test/java/com/capgemini/coedevon/teammanager/PruebaTest.java
+++ src/test/java/com/capgemini/coedevon/teammanager/PruebaTest.java
@@ -1,9 +1,17 @@
 package com.capgemini.coedevon.teammanager;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.capgemini.coedevon.teammanager.utils.UtilsServiceImpl;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class PruebaTest {
 
   @Test
