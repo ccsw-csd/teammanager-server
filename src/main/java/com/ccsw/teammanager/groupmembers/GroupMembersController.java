ecbf77817f3a5bff517b7ecca389c058a1a33830
ivanmr14 <imitro@alumni.uv.es>
2024-02-07 12:43:33 +0100

    Corrección errores tras PR

--- src/main/java/com/ccsw/teammanager/groupmembers/GroupMembersController.java
+++ src/main/java/com/ccsw/teammanager/groupmembers/GroupMembersController.java
@@ -1,12 +1,7 @@
 package com.ccsw.teammanager.groupmembers;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -15,15 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ccsw.teammanager.config.mapper.BeanMapper;
 import com.ccsw.teammanager.groupmembers.model.Detail;
 import com.ccsw.teammanager.groupmembers.model.GroupMembersDto;
-import com.ccsw.teammanager.person.PersonService;
-import com.ccsw.teammanager.person.model.PersonDto;
-import com.ccsw.teammanager.person.model.PersonEntity;
-import com.ccsw.teammanager.personabsence.PersonAbsenceService;
-import com.ccsw.teammanager.personabsence.model.PersonAbsenceDto;
-import com.ccsw.teammanager.personabsence.model.PersonAbsenceEntity;
 
 @RequestMapping(value = "/group_members")
 @RestController
@@ -31,15 +19,6 @@ public class GroupMembersController {
 
     @Autowired
     private GroupMembersService groupMembersService;
-
-    @Autowired
-    private PersonService personService;
-
-    @Autowired
-    private PersonAbsenceService personAbsenceService;
-
-    @Autowired
-    private BeanMapper beanMapper;
 
     /**
      * Método para recuperar registros a partir del group_id
@@ -52,75 +31,7 @@ public class GroupMembersController {
             @RequestParam(name = "start_date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
             @RequestParam(name = "end_date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate) {
 
-        List<GroupMembersDto> members = this.beanMapper.mapList(this.groupMembersService.findByGroupId(groupId),
-                GroupMembersDto.class);
-
-        int workingDays = (int) ChronoUnit.DAYS.between(startDate, endDate) + 1;
-
-        int festives;
-        int others;
-        int vacations;
-
-        List<Detail> details = new ArrayList<>();
-
-        // Formo lista con los ID de los miembros del grupo
-        List<Long> membersId = members.stream().map(GroupMembersDto::getPersonId).collect(Collectors.toList());
-
-        // Recoger nombre de los miembros del grupo
-
-        List<PersonEntity> persons = personService.findAllById(membersId);
-
-        // Recoger las ausencias de todos los miembros del grupo
-
-        List<PersonAbsenceEntity> membersAbsences = personAbsenceService.findAllByPersonIdInAndDateBetween(membersId,
-                Date.valueOf(startDate.toLocalDate()), Date.valueOf(endDate.toLocalDate()));
-
-        for (GroupMembersDto member : members) {
-            Detail detail = new Detail();
-            festives = 0;
-            others = 0;
-            vacations = 0;
-
-            // Obtener datos del miembro
-            Optional<PersonEntity> personOptional = persons.stream()
-                    .filter(person -> person.getId().equals(member.getPersonId())).findFirst();
-
-            personOptional.ifPresent(person -> {
-                detail.setPerson(this.beanMapper.map(person, PersonDto.class));
-                detail.setFullName(person.getName() + " " + person.getLastname());
-            });
-
-            // Obtener las ausencias correspondientes al miembro
-            List<PersonAbsenceEntity> absences = membersAbsences.stream()
-                    .filter(absence -> absence.getPerson().getId().equals(member.getPersonId()))
-                    .collect(Collectors.toList());
-
-            detail.setAbsences(this.beanMapper.mapList(absences, PersonAbsenceDto.class));
-
-            for (PersonAbsenceEntity absence : absences) {
-
-                switch (absence.getAbsence_type()) {
-                case "VAC":
-                    vacations++;
-                    break;
-                case "OTH":
-                    others++;
-                    break;
-                default:
-                    festives++;
-                    break;
-
-                }
-
-            }
-
-            detail.setWorkingDays(workingDays - festives - others - vacations);
-            detail.setFestives(festives);
-            detail.setOthers(others);
-            detail.setVacations(vacations);
-            details.add(detail);
-
-        }
+        List<Detail> details = this.groupMembersService.findDetails(groupId, startDate, endDate);
 
         return details;
     }
--- src/main/java/com/ccsw/teammanager/groupmembers/GroupMembersServiceImpl.java
+++ src/main/java/com/ccsw/teammanager/groupmembers/GroupMembersServiceImpl.java
@@ -1,17 +1,41 @@
 package com.ccsw.teammanager.groupmembers;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ccsw.teammanager.config.mapper.BeanMapper;
+import com.ccsw.teammanager.groupmembers.model.Detail;
+import com.ccsw.teammanager.groupmembers.model.GroupMembersDto;
 import com.ccsw.teammanager.groupmembers.model.GroupMembersEntity;
+import com.ccsw.teammanager.person.PersonService;
+import com.ccsw.teammanager.person.model.PersonDto;
+import com.ccsw.teammanager.person.model.PersonEntity;
+import com.ccsw.teammanager.personabsence.PersonAbsenceService;
+import com.ccsw.teammanager.personabsence.model.PersonAbsenceDto;
+import com.ccsw.teammanager.personabsence.model.PersonAbsenceEntity;
 
 @Service
 @Transactional
 public class GroupMembersServiceImpl implements GroupMembersService {
+
+    @Autowired
+    private BeanMapper beanMapper;
+
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private PersonAbsenceService personAbsenceService;
 
     @Autowired
     GroupMembersRepository groupMembersRepository;
@@ -19,6 +43,81 @@ public class GroupMembersServiceImpl implements GroupMembersService {
     @Override
     public List<GroupMembersEntity> findByGroupId(Long groupId) {
         return groupMembersRepository.findByGroupId(groupId);
+    }
+
+    @Override
+    public List<Detail> findDetails(Long groupId, LocalDateTime startDate, LocalDateTime endDate) {
+
+        List<GroupMembersDto> members = this.beanMapper.mapList(this.findByGroupId(groupId), GroupMembersDto.class);
+
+        int workingDays = (int) ChronoUnit.DAYS.between(startDate, endDate) + 1;
+
+        int festives;
+        int others;
+        int vacations;
+
+        List<Detail> details = new ArrayList<>();
+
+        // Formo lista con los ID de los miembros del grupo
+        List<Long> membersId = members.stream().map(GroupMembersDto::getPersonId).collect(Collectors.toList());
+
+        // Recoger nombre de los miembros del grupo
+
+        List<PersonEntity> persons = personService.findAllById(membersId);
+
+        // Recoger las ausencias de todos los miembros del grupo
+
+        List<PersonAbsenceEntity> membersAbsences = personAbsenceService.findAllByPersonIdInAndDateBetween(membersId,
+                Date.valueOf(startDate.toLocalDate()), Date.valueOf(endDate.toLocalDate()));
+
+        for (GroupMembersDto member : members) {
+            Detail detail = new Detail();
+            festives = 0;
+            others = 0;
+            vacations = 0;
+
+            // Obtener datos del miembro
+            Optional<PersonEntity> personOptional = persons.stream()
+                    .filter(person -> person.getId().equals(member.getPersonId())).findFirst();
+
+            personOptional.ifPresent(person -> {
+                detail.setPerson(this.beanMapper.map(person, PersonDto.class));
+                detail.setFullName(person.getName() + " " + person.getLastname());
+            });
+
+            // Obtener las ausencias correspondientes al miembro
+            List<PersonAbsenceEntity> absences = membersAbsences.stream()
+                    .filter(absence -> absence.getPerson().getId().equals(member.getPersonId()))
+                    .collect(Collectors.toList());
+
+            detail.setAbsences(this.beanMapper.mapList(absences, PersonAbsenceDto.class));
+
+            for (PersonAbsenceEntity absence : absences) {
+
+                switch (absence.getAbsence_type()) {
+                case "VAC":
+                    vacations++;
+                    break;
+                case "OTH":
+                    others++;
+                    break;
+                default:
+                    festives++;
+                    break;
+
+                }
+
+            }
+
+            detail.setWorkingDays(workingDays - festives - others - vacations);
+            detail.setFestives(festives);
+            detail.setOthers(others);
+            detail.setVacations(vacations);
+            details.add(detail);
+
+        }
+
+        return details;
     }
 
 }
